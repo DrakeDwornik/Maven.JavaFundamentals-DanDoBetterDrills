919a1beb48a790c9ee5c6b6dd43d506f49750001
Drake Dwornik <drake@drakedwornik.com>
2022-01-24 15:00:41 -0500

    predict utils passing

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
@@ -10,7 +10,9 @@ public class PredicateUtilities {
      * @return true if `x` is greater than `y`
      */
     public Boolean isGreaterThan(int x, int y) {
-        return null;
+
+        boolean result = x > y;
+        return result;
     }
 
     /**
@@ -19,7 +21,9 @@ public class PredicateUtilities {
      * @return true if `x` is less than `y`
      */
     public Boolean isLessThan(int x, int y) {
-        return null;
+
+        boolean result = x < y;
+        return result;
     }
 
     /**
@@ -28,7 +32,9 @@ public class PredicateUtilities {
      * @return true if `x` is greater than or equal to `y`
      */
     public Boolean isGreaterThanOrEqualTo(int x, int y) {
-        return null;
+
+        boolean result = x >= y;
+        return result;
     }
 
     /**
@@ -37,7 +43,10 @@ public class PredicateUtilities {
      * @return true if `x` is less than or equal to `y`
      */
     public Boolean isLessThanOrEqualTo(int x, int y) {
-        return null;
+
+        boolean result = x <= y;
+
+        return result;
     }
 
 
@@ -45,14 +54,16 @@ public class PredicateUtilities {
      * @return true
      */
     public Boolean returnTrue() {
-        return null;
+
+        return true;
     }
 
     /**
      * @return false
      */
     public Boolean returnFalse() {
-        return null;
+
+        return false;
     }
 
 }
